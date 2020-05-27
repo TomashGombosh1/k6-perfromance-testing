22524c90fbb422a8dda3f293d7fabd70d1d73e97
TomashGombosh <blackjneco@gmail.com>
2020-05-27 12:13:04 +0300

    fix

---
+++ src/urls.js
@@ -0,0 +1,57 @@
+export default urls = {
+    main : {
+        main : [
+            "/",
+            "/why-qatestlab",
+            "/company/contacts/"
+        ]
+    },
+
+    services: {
+        manual : [
+            "/services/manual-testing/functional-testing",
+            "/services/manual-testing/regression-testing",
+            "/services/manual-testing/ux-usability-testing",
+            "/services/manual-testing/compatibility-testing",
+            "/services/manual-testing/configuration-testing",
+            "/services/manual-testing/system-testing",
+            "/services/manual-testing/localization-testing",
+            "/services/manual-testing/gui-testing",
+        ],
+        auto : [
+            "/services/test-automation/performance-testing",
+            "/services/test-automation/penetration-testing"
+        ],
+        special : [
+            "/services/special-offer/magento-test-suite",
+            "/special-offer/online-music-streaming",
+            "/services/special-offer/voice-technologies/"
+        ]
+    },
+    resources : {
+        whitepapers : [
+            "/future-of-mobile-automation", 
+            "/assets/Uploads/Whitepaper.-Guide-to-Test-Automation-Tools-2017.pdf",
+            "/assets/Independent-Testing-Providers-vs.-Software-Houses-Quick-Guide-to-selecting-a-proper-vendor.pdf"
+        ],
+        webinars : [
+            "/resources/webinars/independent-testing-crm",
+            "/resources/webinars/automated-testing-future",
+            "/resources/webinars/what-we-learned",
+            "/resources/webinars/qa-process-startup",
+            "/resources/webinars/it-business-opportunities",
+            "/resources/webinars/testing-without-testers",
+            "/resources/webinars/automation-too-late",
+            "/resources/webinars/test-case-management",
+            "/resources/webinars/hybrid-mobile-strategy",
+            "/resources/webinars/measure-code-coverage"
+        ],
+        webinarsPages : [
+            "resources/webinars/page-2",
+            "resources/webinars/page-3",
+            "resources/webinars/page-4",
+            "resources/webinars/page-5",
+            "resources/webinars/page-6"
+        ]
+    }
+}
